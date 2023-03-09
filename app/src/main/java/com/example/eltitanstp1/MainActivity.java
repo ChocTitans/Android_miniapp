77a0b6e26b7a62596a8b344c418f49c163618bb1
ChocTitans <sampcfrpg@live.fr>
2023-03-09 23:20:33 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/eltitanstp1/MainActivity.java
+++ app/src/main/java/com/example/eltitanstp1/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.eltitanstp1;
+package com.example.application1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView text = findViewById(R.id.txt1);
-        text.setText("eLTitans");
+        Log.d("MainActivity", "The Application is onCreate state");
     }
-    g
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "The Application is onStart state");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "The Application is onResume state");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "The Application is onPause state");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "The Application is onStop state");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "The Application onDestroy state ");
+    }
 }
